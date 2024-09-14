ad4dc1d9b823fe6918dcd9c5631ee5a614c106c5
vishalshingate <vishalshingate26@gmail.com>
2024-09-15 00:21:48 +0530

    2.Run Sample APplication, without any spring wiring, this is simple example, without spring, we will see how spring help us.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import com.MyProject.com.MyProject.model.SpeakerService;
+import com.MyProject.com.MyProject.model.SpeakerServiceImpl;
 
 public class Application {
+
+    public static void main(String[] args) {
+        SpeakerService speakerService = new SpeakerServiceImpl();
+        System.out.println(speakerService.findAll().get(0).getFirstName());
+    }
 }
