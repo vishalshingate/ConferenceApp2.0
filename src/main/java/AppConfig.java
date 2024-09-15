232ed84febe3d60c16ad9ee249574f2dd39e56ef
vishalshingate <vishalshingate26@gmail.com>
2024-09-15 12:52:26 +0530

    2.Spring Configuration using java: Created Configuration, Created first Bean for a SpeakerService, it returns the SpeakerServiceImpl referenced by SpeakerService interface.

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,3 +1,13 @@
+import com.MyProject.com.MyProject.model.SpeakerService;
+import com.MyProject.com.MyProject.model.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        return new SpeakerServiceImpl();
+    }
 
 }
