39a922fda404b6352ef3d10d0be3a5591941901e
vishalshingate <vishalshingate26@gmail.com>
2024-10-20 18:31:39 +0530

    7.Advanced Bean configuration: Added bean PostConstruct annotation to project, for that added dependency.

--- src/main/java/com/MyProject/service/SpeakerServiceImpl.java
+++ src/main/java/com/MyProject/service/SpeakerServiceImpl.java
@@ -2,6 +2,7 @@ package com.MyProject.service;
 
 import com.MyProject.model.Speaker;
 import com.MyProject.repository.SpeakerRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
@@ -18,11 +19,19 @@ public class SpeakerServiceImpl implements SpeakerService {
     public SpeakerServiceImpl() {
         System.out.println(" We are in SpeakerServiceImpl no org constructor.");
     }
+
     @Autowired
     public SpeakerServiceImpl(SpeakerRepository repository) {
         System.out.println(" We are in SpeakerServiceImpl Repository constructor.");
         this.repository = repository;
     }
+
+    @PostConstruct
+    private  void initialize() {
+        System.out.println(" We are called it after the constructors.");
+    }
+
+
 
     @Override
     public List<Speaker> findAll() {
